9a4605903e99df1da552fcb95f7ee5a626b580d2
codebyoshama <162036964+codebyoshama@users.noreply.github.com>
2025-06-26 23:15:34 -0500

    Best time to buy and sell stock 2

--- src/BestTimeToBuyAndSellStock.java
+++ src/BestTimeToBuyAndSellStock.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class BestTimeToBuyAndSellStock {
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int price: prices) {
+            if(price < minPrice) {
+                minPrice = price;
+            }else {
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }
+        return maxProfit;
+
+    }
+
 }
+
